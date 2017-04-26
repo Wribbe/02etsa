608fdfde3c9fcf56d68c7bda20dff0925d844578
Stefan Eng <eng_steff@hotmail.com>
2017-04-26 13:24:24 +0200

    Add javaFX hello world.

--- src/etsa02/Main.java
+++ src/etsa02/Main.java
@@ -1,8 +1,34 @@
 package etsa02;
 
-class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 	public static void main(String args[]) 
 	{
-		System.out.println("HELLO WORLD!");
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage)
+	{
+		primaryStage.setTitle("Hello World");
+
+		Button button = new Button();
+		button.setText("Say 'Hello World'");
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			public  void handle(ActionEvent event) {
+				System.out.println("Hello World!");
+			}
+		});
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(button);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
 	}
 }
