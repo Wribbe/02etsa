cb0dec2a9250395315acb866f3320c5c91a6a0b5
Stefan Eng <eng_steff@hotmail.com>
2017-04-26 13:47:46 +0200

    Two input fields created without button.

--- src/etsa02/Main.java
+++ src/etsa02/Main.java
@@ -1,11 +1,16 @@
 package etsa02;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,19 +21,35 @@ public class Main extends Application {
 	
 	public void start(Stage primaryStage)
 	{
-		primaryStage.setTitle("Hello World");
-
-		Button button = new Button();
-		button.setText("Say 'Hello World'");
-		button.setOnAction(new EventHandler<ActionEvent>() {
-			public  void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
-		});
+		primaryStage.setTitle("JavaFX Welcome");
 		
-		StackPane root = new StackPane();
-		root.getChildren().add(button);
-		primaryStage.setScene(new Scene(root, 300, 250));
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		
+		Text scenetitle = new Text("Welcome");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		
+		Scene scene = new Scene(grid, 300, 275);
+		grid.add(scenetitle, 0, 0, 2, 1);
+		
+		Label userName = new Label("User Name:");
+		grid.add(userName, 0, 1);
+		
+		TextField userTextField = new TextField();
+		grid.add(userTextField, 1, 1);
+		
+		Label password = new Label("Password");
+		grid.add(password, 0, 2);
+		
+		PasswordField passwordBox = new PasswordField();
+		grid.add(passwordBox, 1, 2);
+
+		primaryStage.setScene(scene);
+
 		primaryStage.show();
 	}
 }
