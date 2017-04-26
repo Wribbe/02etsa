dc801e711a00d6b00ae2b4fcc9a87d109a2488d7
Stefan Eng <eng_steff@hotmail.com>
2017-04-26 14:05:00 +0200

    Add action when pressing button with mouse.

--- src/etsa02/Main.java
+++ src/etsa02/Main.java
@@ -1,6 +1,8 @@
 package etsa02;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,6 +12,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -58,6 +61,13 @@ public class Main extends Application {
 		
 		final Text actiontarget = new Text();
 		grid.add(actiontarget, 1, 6);
+		
+		signIn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				actiontarget.setFill(Color.FIREBRICK);
+				actiontarget.setText("Sign in button pressed.");
+			}
+		});
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
