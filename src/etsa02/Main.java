5e62ecb13795aaa08d6d415a695b56961f58161b
wribbe <eng_steff@hotmail.com>
2017-05-04 17:04:41 +0200

    Remove old imports.

--- src/etsa02/Main.java
+++ src/etsa02/Main.java
@@ -14,9 +14,6 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
