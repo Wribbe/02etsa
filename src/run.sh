07369caa7b66435d0df91ef5b173e3b8736010ca
wribbe <eng_steff@hotmail.com>
2017-05-09 21:21:11 +0200

    Compile everything with run.sh.

--- src/run.sh
+++ src/run.sh
@@ -4,11 +4,14 @@ directory="$1"
 echo ${directory}
 
 cd ${directory}
-javac Main.java
+javac *.java
 compilation_result=$?
 if [ "$compilation_result" != 0 ]; then
     echo "Compilation error.. aborting."
     exit
 fi
 cd ..
-java ${directory}.Main
+
+command="java ${directory}.Main"
+echo "Running command: ${command}"
+${command}
