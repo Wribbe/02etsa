3fc8eff8ff3b57bdad733da1907bdb5aa5d0b06c
wribbe <eng_steff@hotmail.com>
2017-05-15 19:56:21 +0200

    Instantiate graphical fake-printer.

--- src/etsa02/Main.java
+++ src/etsa02/Main.java
@@ -479,6 +479,9 @@ public class Main extends Application {
 
     public void start(Stage primaryStage)
     {
+    	BarcodePrinterTestDriver printer = new BarcodePrinterTestDriver("mupp", 10, 10);
+    	
+    	printer.printBarcode("12345");
         stage_main = primaryStage;
         stage_main.setWidth(LOGIN_WIDTH);
         stage_main.setHeight(LOGIN_HEIGHT);
